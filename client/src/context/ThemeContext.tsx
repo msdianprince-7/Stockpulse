0e9f03e760437c2c878f527a6bac1c982d08c84c
Priyansh Nandwana <nandwana.prince103@gmail.com>
2026-03-28 13:57:59 +0530

    feat: default to dark mode

--- client/src/context/ThemeContext.tsx
+++ client/src/context/ThemeContext.tsx
@@ -17,7 +17,7 @@ const ThemeContext = createContext<ThemeContextType>({
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState<Theme>('light');
+  const [theme, setTheme] = useState<Theme>('dark');
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
